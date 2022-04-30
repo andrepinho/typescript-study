b09f2c759ef44e577f51412bb23259ac393efee2
André Pinho <andresouzapinho@gmail.com>
2022-04-30 15:21:12 -0300

    No BS TS #4 - Function Overloading in Typescript

---
+++ parseCoordinate.ts
@@ -0,0 +1,33 @@
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+function parseCoordinate(obj: Coordinate): Coordinate;
+function parseCoordinate(str: string): Coordinate;
+function parseCoordinate(x: number, y: number): Coordinate;
+function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+  let coord: Coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  if (typeof arg1 === "string") {
+    arg1.split(",").forEach((str) => {
+      const [key, value] = str.split(":");
+      coord[key as "x" | "y"] = parseInt(value, 10);
+    });
+  } else if (typeof arg1 === "object") {
+    coord = { ...(arg1 as Coordinate) };
+  } else {
+    coord = {
+      x: arg1 as number,
+      y: arg2 as number,
+    };
+  }
+
+  return coord;
+}
+console.log(parseCoordinate(1, 2));
+console.log(parseCoordinate({ x: 3, y: 4 }));
+console.log(parseCoordinate("x:5,y:6"));

1a36a618bc564ea41bc28f1d78b19a966d2409ab
André Pinho <andresouzapinho@gmail.com>
2022-05-09 22:13:56 -0300

    No BS TS #14 - Mapped Types in Typescript

---
+++ mapped.ts
@@ -0,0 +1,48 @@
+type MyFlexibleDog = {
+  name: string;
+  [key: string]: string | number;
+};
+
+const dog: MyFlexibleDog = {
+  name: "Argos",
+  breed: "German shepherd",
+  age: 11,
+};
+
+interface DogInfo {
+  name: string;
+  age: number;
+}
+
+type OptionsFlags<Type> = {
+  [Property in keyof Type]: boolean;
+};
+
+type DogInfoOptions = OptionsFlags<DogInfo>;
+
+type Listeners<T> = {
+  [Property in keyof T as `on${Capitalize<string & Property>}Change`]?: (
+    newValue: T[Property]
+  ) => void;
+} & {
+  [Property in keyof T as `on${Capitalize<
+    string & Property
+  >}Delete`]?: () => void;
+};
+
+function listenToObject<T>(obj: T, listeners: Listeners<T>): void {
+  throw "Needs to be implemented";
+}
+
+const lg: DogInfo = {
+  name: "LG",
+  age: 13,
+};
+
+type DogInfoListeners = Listeners<DogInfo>;
+
+listenToObject(lg, {
+  onNameChange: (v: string) => {},
+  onAgeChange: (v: number) => {},
+  onAgeDelete: () => {},
+});

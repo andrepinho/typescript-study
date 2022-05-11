fdad5f7e34dd077150655ea121f688e1669f9e7a
André Pinho <andresouzapinho@gmail.com>
2022-05-11 15:16:21 -0300

    Change test function to use lodash isEqual

--- leet-code/test-utils.ts
+++ leet-code/test-utils.ts
@@ -1,12 +1,23 @@
+import lodash from "lodash";
+
+function isEqual<T>(arg1: T, arg2: T) {
+  return lodash.isEqual(arg1, arg2);
+}
+
 function test<InputType, OutputType>(
   func: (i: InputType) => OutputType,
   testCases: readonly [InputType, OutputType][]
 ): void {
   console.time(func.name);
   testCases.forEach(([input, expectedOutput]) => {
-    const result = func(input);
-    if (result !== expectedOutput)
-      console.log(`Fail. Expected ${expectedOutput} but got ${result}`, "\n");
+    const actualOutput = func(input);
+    if (!isEqual(actualOutput, expectedOutput))
+      console.log(
+        `Fail. Expected ${expectedOutput} but got ${actualOutput}. Input was: ${JSON.stringify(
+          input
+        )}`,
+        "\n"
+      );
   });
   console.timeEnd(func.name);
 }
